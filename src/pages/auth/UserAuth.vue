815ad6e552b030a0506cbc90ebe0c3bc466a563f
elmi-elmi <shahrokhelmi@gmail.com>
2021-10-01 10:45:42 +0330

    video 6

---
+++ src/pages/auth/UserAuth.vue
@@ -0,0 +1,98 @@
+<template>
+  <base-card>
+    <form @submit.prevent="submitForm">
+      <div class="form-control">
+        <label for="email">Email</label>
+        <input type="email" v-model.trim="email" />
+      </div>
+      <div class="form-control">
+        <label for="password">Password</label>
+        <input type="password" v-model.trim="password" />
+      </div>
+      <p v-if="!formIsValid">what the f this shit</p>
+      <base-button>{{ submitButtonCaption }}</base-button>
+      <base-button type="button" mode="flat" @click="switchForm">
+        {{ switchModeButtonCaption }}</base-button
+      >
+    </form>
+  </base-card>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      email: '',
+      password: '',
+      mode: 'login',
+      formIsValid: true
+    };
+  },
+  computed: {
+    submitButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Login';
+      } else {
+        return 'Signup';
+      }
+    },
+    switchModeButtonCaption() {
+      if (this.mode === 'login') {
+        return 'Instead signup';
+      } else {
+        return 'Instead login';
+      }
+    }
+  },
+  methods: {
+    switchForm() {
+      if (this.mode === 'login') {
+        this.mode = 'signup';
+      } else {
+        this.mode = 'login';
+      }
+    },
+    submitForm() {
+      if (this.email === '') {
+        return false;
+      }
+      if (this.password === '') {
+        return false;
+      }
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  margin: 1rem;
+  padding: 1rem;
+}
+
+.form-control {
+  margin: 0.5rem 0;
+}
+
+label {
+  font-weight: bold;
+  margin-bottom: 0.5rem;
+  display: block;
+}
+
+input,
+textarea {
+  display: block;
+  width: 100%;
+  font: inherit;
+  border: 1px solid #ccc;
+  padding: 0.15rem;
+}
+
+input:focus,
+textarea:focus {
+  border-color: #3d008d;
+  background-color: #faf6ff;
+  outline: none;
+}
+</style>
